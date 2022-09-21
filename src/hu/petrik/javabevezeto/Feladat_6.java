c52a5659c82351c0d5f8128abdd65dc4b0827758
hyimred <ptrk.csak@gmail.com>
2022-09-21 20:15:29 +0200

    Feladat 6

--- src/hu/petrik/javabevezeto/Feladat_6.java
+++ src/hu/petrik/javabevezeto/Feladat_6.java
@@ -2,4 +2,28 @@ package hu.petrik.javabevezeto;
 
 import java.util.Scanner;
 public class Feladat_6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kérem adja meg a dolgozat pontszámát: ");
+        int pontszam = sc.nextInt();
+
+        if (pontszam < 0 || pontszam > 100) {
+            System.out.println("Nincs ilyen pontszám");
+        } else if (pontszam < 43) {
+            System.out.println("Elégtelen");
+        } else if (pontszam < 58) {
+            System.out.println("Elégséges");
+        } else if (pontszam < 73) {
+            System.out.println("Közepes");
+        } else if (pontszam < 88) {
+            System.out.println("Jó");
+        } else if (pontszam <= 100) {
+            System.out.println("Jeles");
+        } else {
+            System.out.println("Nem egész számot írt be");
+        }
+
+    }
+
 }
