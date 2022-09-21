c4e07e88bff436a06df236ee2a0a35375e54a6b5
hyimred <ptrk.csak@gmail.com>
2022-09-21 21:23:20 +0200

    Feladat 17

--- src/hu/petrik/javabevezeto/Feladat_17.java
+++ src/hu/petrik/javabevezeto/Feladat_17.java
@@ -2,5 +2,16 @@ package hu.petrik.javabevezeto;
 
 import java.util.Scanner;
 public class Feladat_17 {
+    public static void main(String[] args) {
+        double[] tomb1 = {26.3,11.4,91.0,34.2,76.9};
+        double[] tomb2 = {32.1,10.5,83.7,3.6,99.99};
+        double[] tomb3 = {0,0,0,0,0};
+
+        for (int i = 0; i < 5; i++) {
+            tomb3[i] = tomb1[i]+tomb2[i];
+            System.out.print(tomb3[i] + ", ");
+        }
+
+    }
 }
 
