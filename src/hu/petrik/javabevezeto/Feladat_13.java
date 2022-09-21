b5ea1fc845cf356e319d926fb9ada498a2ad3e57
hyimred <ptrk.csak@gmail.com>
2022-09-21 20:48:28 +0200

    Feladat 13

--- src/hu/petrik/javabevezeto/Feladat_13.java
+++ src/hu/petrik/javabevezeto/Feladat_13.java
@@ -1,5 +1,31 @@
 package hu.petrik.javabevezeto;
 
+import java.util.Random;
 import java.util.Scanner;
 public class Feladat_13 {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+        int random = new Random().nextInt(100) + 1;
+        int counter = 0;
+
+        while (true) {
+            System.out.print("Tippelj egy számot 1 és 100 között : ");
+
+            Scanner sc = new Scanner(System.in);
+            int tipp = sc.nextInt();
+
+            counter++;
+
+            if (tipp > random) {
+                System.out.println("A szám kissebb, mint " + tipp);
+            } else if (tipp < random) {
+                System.out.println("A szám nagyobb, mint " + tipp);
+            } else {
+                System.out.println("Gratulálok, nyertél! Tippek száma: " + counter);
+                break;
+            }
+        }
+    }
 }
