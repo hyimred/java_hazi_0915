da0e99d0fd4342e79dd8199d05f4c89ce4ccb555
hyimred <ptrk.csak@gmail.com>
2022-09-21 20:26:22 +0200

    Feladat 11

--- src/hu/petrik/javabevezeto/Feladat_11.java
+++ src/hu/petrik/javabevezeto/Feladat_11.java
@@ -2,4 +2,16 @@ package hu.petrik.javabevezeto;
 
 import java.util.Scanner;
 public class Feladat_11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kérem adjon meg egy pozitív egész számot: ");
+        int N = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i < N+1; i+=2) {
+            sum += i;
+        }
+
+        System.out.println("Összeg: " + sum);
+    }
 }
